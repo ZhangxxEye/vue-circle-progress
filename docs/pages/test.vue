0c655f2de1d917c94f6d87a633ba02d4294dfbb9
zhangxiaoxia <zhangxiaoxia@51talk.com>
2019-04-15 16:58:55 +0800

    环形进度条

--- docs/pages/test.vue
+++ docs/pages/test.vue
@@ -3,86 +3,106 @@
         <h1 class="title">vue-list</h1>
         <div class="features">
             <div class="box">
-                <demo :percent="50"></demo>
+                <demo :percent="0.5" :width="80" :height="80"></demo>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import demo from '../../src/index';
-    import axois from 'axios';
-    export default {
-        name: 'ProjectFeatures',
-        data() {
-            return {
-                list: [],
-                loading: false,
-                finished: false,
-                error: false
-            };
-        },
-        components: {
-          demo,
-        },
-        mounted () {
-        },
-        methods: {
-            handleClick (value) {
-                console.log('selected', value);
-            },
-            getLists () {
-                axois.get('http://172.16.0.45:7300/mock/5c88c2241d2cb328eddca711/components/api/list').then(r => {
-                    this.list.push(...r.data.data);
-                    if (this.list.length >= 90) {
-                        this.finished = true;
-                    }
-                }).catch(e => {
-                    this.error = true;
-                }).finally(() => {
-                    this.loading = false;
-                });
-            },
-            onLoad() {
-                this.getLists();
-                // 异步更新数据
-                /*setTimeout(() => {
-                    for (let i = 0; i < 10; i++) {
-                        this.list.push(this.list.length + 1);
-                    }
-                    // 加载状态结束
-                    // this.loading = false;
-                    this.error = true;
+  import demo from '../../src/index';
+  import axois from 'axios';
 
-                    // 数据全部加载完成
-                    if (this.list.length >= 90) {
-                        this.finished = true;
-                    }
-                }, 500);*/
+  export default {
+    name: 'ProjectFeatures',
+    data() {
+      return {
+        list: [],
+        loading: false,
+        finished: false,
+        error: false,
+        num: [0.1, 0.2, 0.3, 0.6, 0.8, 0.9, 1],
+        percent: 0,
+        index: 0
+      };
+    },
+    components: {
+      demo,
+    },
+    mounted() {
+      // this.timer();
+    },
+    methods: {
+      timer() {
+        let self = this;
+        let time = setInterval(function () {
+
+          self.percent = self.num[self.index];
+          console.log('ssssss', self.percent);
+          if (self.index == 6) clearInterval(time);
+          self.index ++;
+        }, 1000);
+      },
+      handleClick(value) {
+        console.log('selected', value);
+      },
+      getLists() {
+        axois.get('http://172.16.0.45:7300/mock/5c88c2241d2cb328eddca711/components/api/list').then(r => {
+          this.list.push(...r.data.data);
+          if (this.list.length >= 90) {
+            this.finished = true;
+          }
+        }).catch(e => {
+          this.error = true;
+        }).finally(() => {
+          this.loading = false;
+        });
+      },
+      onLoad() {
+        this.getLists();
+        // 异步更新数据
+        /*setTimeout(() => {
+            for (let i = 0; i < 10; i++) {
+                this.list.push(this.list.length + 1);
             }
-        }
-    };
+            // 加载状态结束
+            // this.loading = false;
+            this.error = true;
+
+            // 数据全部加载完成
+            if (this.list.length >= 90) {
+                this.finished = true;
+            }
+        }, 500);*/
+      }
+    }
+  };
 </script>
 
 <style lang="scss">
     @import '~colors';
-    .test-box{
+
+    .test-box {
         width: 100%;
         height: 300px;
         overflow-y: scroll;
     }
-    .box{
+
+    .box {
         width: 300px;
         height: 300px;
     }
-    .outer-container{
+
+    .outer-container {
         height: 200px;
         width: 300px;
     }
-    .item{
-        flex:0 0 22%;
+
+    .item {
+        flex: 0 0 22%;
     }
-    .tab-container{
+
+    .tab-container {
         padding: 10px;
     }
 
--- src/index.vue
+++ src/index.vue
@@ -1,118 +1,98 @@
 <template>
-    <div class="circle-container">
-        <div class="circle-bar" id="circle-bar">
-            <div class="circle-bar-left"></div>
-            <div class="circle-bar-right"></div>
-            <!-- 遮罩层，显示百分比 -->
-            <div class="mask">
-                <span class="percent"></span>
-            </div>
-        </div>
+    <div class="progress" :style="progressStyle">
+        <canvas id="canvas1" :height="height" :width="width"></canvas>
     </div>
 </template>
 
 <script>
-  import * as utils from './common/utils/utils';
-
   export default {
     name: "vue-circle-progress",
+    data () {
+      return {
+        progressStyle: {
+          width: this.width + 'px',
+          height: this.height + 'px',
+          backgroundColor: this.progressBarColor
+        },
+        canvas: null,
+        canvasContext: null
+      }
+    },
     props: {
+      width: {
+        type: Number,
+        default: 68
+      },
+      height: {
+        type: Number,
+        default: 68
+      },
       percent: {
         type: Number,
         default: 0
       },
       progressColor: {
         type: String,
-        default: ''
+        default: '#FFFFFF'
       },
       progressBarColor: {
         type: String,
-        default: ''
+        default: '#FFE83C'
       }
     },
+    watch: {
+      // percent () {
+      //   console.log('change ', this.percent);
+      //   console.log('context ', this.canvasContext);
+      //   this.drawCircle(this.percent);
+      // }
+    },
     mounted() {
-      let circleBar = document.getElementById('circle-bar');
-      let color = utils.css(circleBar, 'background-color');
-      let left_circle = circleBar.getElementsByClassName('circle-bar-left')[0];
-      let right_circle = circleBar.getElementsByClassName('circle-bar-right')[0];
+      this.initCanvas();
+      this.drawCircle(this.percent);
+    },
+    methods: {
+      $id(id) {
+        return typeof id === "string" ? document.getElementById(id) : id;
+      },
+      initCanvas () {
+        this.canvas = this.$id("canvas1");
+        // let canvas2 = this.$id("canvas2");
+        this.canvasContext = this.canvas.getContext("2d"); //2d对象
+        this.canvasContext.lineWidth = 2;
+        this.canvasContext.strokeStyle = this.progressColor;
+      },
+      drawCircle (percent) {
+        console.log('drawCircle', percent);
+        let x = this.width / 2;
+        let angle = percent * 360; //角度
 
-      if (this.percent <= 50) {
-        let rotate = 'rotate(' + (this.percent * 3.6) + 'deg)';
-        utils.css3(right_circle, 'transform', rotate);
-      } else {
-        let rotate = 'rotate(' + ((this.percent - 50) * 3.6) + 'deg)';
-        utils.css(right_circle, 'background-color', color);//背景色设置为进度条的颜色
-        utils.css3(right_circle, 'transform', 'rotate(0deg)');//右侧不旋转
-        utils.css3(left_circle, 'transform', rotate);//左侧旋转
+        this.canvasContext.beginPath();
+        console.log('angle * Math.PI / 180', angle);
+        this.canvasContext.arc(x, x, x-3, 0, angle * Math.PI / 180, false);
+        this.canvasContext.clearRect(0, 0, x, x); //走完一次清空一次
+        this.canvasContext.stroke();
+        this.canvasContext.closePath();
+        this.canvasContext.rotate(90 * Math.PI / 180) //旋转的弧度
+        this.canvasContext.closePath();
+        this.canvasContext.restore(); //返回之前保存过的路径状态和属性
       }
     }
   }
 </script>
 
 <style scoped lang="less">
-    /*支持IE9及以上*/
-    .circle-container{
-        width: 68px;
-        height: 68px;
-        background-color: #FFE83C;
-        position: relative;
-        border-radius: 50%;
-    }
-    .circle-bar {
-        /*font-size: 200px;*/
-        width: 64px;
-        height: 64px;
+    .progress {
         position: absolute;
-        left: 2px;
-        top: 2px;
-        background-color: #FFFFFF;
-    }
-
-    .circle-bar-left, .circle-bar-right {
-        width: 64px;
-        height: 64px;
-        background-color: #FFE83C;
-    }
-
-    /*
-        这里采用clip剪切了圆，实现左右两个半圆，右半圆在后面，因此在更上一层，
-        clip的用法参考：http://www.w3school.com.cn/cssref/pr_pos_clip.asp
-     */
-    .circle-bar-right {
-        clip: rect(0, auto, auto, .5em);
-    }
-
-    .circle-bar-left {
-        clip: rect(0, .5em, auto, 0);
-    }
-
-    .mask {
-        width: 0.8em;
-        height: 0.8em;
-        background-color: #FFE83C;
-        text-align: center;
-        line-height: 0.2em;
-        color: rgba(0, 0, 0, 0.5);
-    }
-
-    .mask :first-child {
-        font-size: 0.3em;
-        height: 0.8em;
-        line-height: 0.8em;
-        display: block;
-    }
-
-    /*所有的后代都水平垂直居中，这样就是同心圆了*/
-    .circle-bar * {
-        position: absolute;
-        left: 2px;
-        top: 2px;
-        margin: auto;
-    }
-
-    /*自身以及子元素都是圆*/
-    .circle-bar, .circle-bar > * {
+        transform: rotate(-90deg);
         border-radius: 50%;
     }
 
+    #canvas1 {
+        position: absolute;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+    }
 </style>
